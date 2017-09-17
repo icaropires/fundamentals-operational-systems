315b308632127f6f93dbda916f52a028be5c2b71
ArthurTemporim <arthurrtl@gmail.com>
2017-09-17 20:37:19 -0300

    Create host_b.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>
    Signed-off-by: Icaro Pires <icaropiresdf@gmail.com>

--- inter_process_communication/host_b/src/main.c
+++ inter_process_communication/host_b/src/main.c
@@ -1,7 +1,14 @@
-#include "process.h"
-#include <stdio.h>
+#include "host_b.h"
 
 int main() {
-	printf("Main file\n");
+	int msqid = create_message_queue(FILE_PATH, ARB_CHAR_B);
+
+	Message msg;
+	
+	fprintf(stdout, "Receiving message in HOST B...\n");
+	receiving_message(msqid, &msg, ARB_NUMBER, FLAG);
+
+	fprintf(stdout, "%s\n", msg.msg);
+
 	return 0;
 }
