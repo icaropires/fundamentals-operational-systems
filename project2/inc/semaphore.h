8c52b40b03f1f4a72ef23771f52509d430ae20a5
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-11 15:11:18 -0300

    Improve semaphore functions and add main for tests
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- project2/inc/semaphore.h
+++ project2/inc/semaphore.h
@@ -4,6 +4,8 @@
 #include "utils.h"
 #include <sys/sem.h>
 
+#define IGNORED_VALUE 0
+
 typedef union semun {
     int val;
     struct semid_ds *buf;
@@ -15,16 +17,16 @@ int create_semaphores(char *file_path, int sem_flags, int sem_num);
 
 void remove_semaphores(int semid);
 
-int initialize_semaphores(int semid, int sem_num);
+int initialize_semaphores(int semid, int sem_num, int value);
 
-int get_ready_semaphores(char *file_path, int sem_flags, int sem_num);
+int get_ready_semaphores(int sem_num);
 
 void check_sem_th_limit(int semid, int sem_th);
 
 void semaphore_wait_for_zero(int semid, int sem_th);
 
-void binary_semaphore_up(int semid, int sem_th);
+void up(int semid, int sem_th);
 
-void binary_semaphore_down(int semid, int sem_th);
+void down(int semid, int sem_th);
 
 #endif  // PROJECT2_INC_SEMAPHORE_H_
--- project2/src/main.c
+++ project2/src/main.c
@@ -1,57 +1,22 @@
 #include "semaphore.h"
 #include "message.h"
 #include "kid.h"
-#include <signal.h>
-
-int semid;
-
-void handle_sigint(int signal){
-	printf("%d\n", semid);
-	remove_semaphores(semid);
-	exit(0);
-}
 
 int main(int argc, char** argv) {
 	generate_arb_file();
-
-	semid = create_semaphores(ARB_FILE, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR, 1);
+	int semid = get_ready_semaphores(10);
 
     pid_t pid = fork();
 
 	if(pid){
-		signal(2, handle_sigint);
-		int count = 0;
-
-		while(1){
-			printf("pai\n");
-			sleep(1);
-
-			if(count == 5){
-				fprintf(stderr, "pai: %d\n", count);
-				binary_semaphore_up(semid, 0);
-			}
-			count++;
-		}
-		kill(pid, SIGINT);
+		remove_semaphores(semid);
+		fprintf(stderr, "Ending parent\n");
 	} else{
-		int count = 0;
-		while(1){
-			printf("filho\n");
-			sleep(1);
-
-			if(count == 2){
-				fprintf(stderr, "filho: %d\n", count);
-				binary_semaphore_down(semid, 0);
-			}
-
-			count++;
-		}
+		fprintf(stderr, "Ending kid\n");
 	}
+	return 0;
     // thinking_kid(pid);
     // waiting_kid(pid);
     // acrossing_kid(pid);
-
-	fprintf(stdout, "I'm working\n");
-
-    return 0;
+	
 }
