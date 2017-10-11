348ffb8d4c44eb89cc48c2862431c443de50e9d2
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-11 15:11:18 -0300

    Improve semaphore functions and add main for tests
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- project2/src/semaphore.c
+++ project2/src/semaphore.c
@@ -18,19 +18,17 @@ int create_semaphores(char *file_path, int sem_flags, int sem_num) {
 }
 
 void remove_semaphores(int semid){
-    const int IGNORED_VALUE = 0;
-
     if(semctl(semid, IGNORED_VALUE, IPC_RMID)){
         perror("Wasn't possible to remove semaphore");
     }
 }
 
 // Ready for binary semaphore
-int initialize_semaphores(int semid, int sem_num) {
+int initialize_semaphores(int semid, int sem_num, int value) {
     Semun argument;
 
     for(int i = 0; i < sem_num; ++i){
-        argument.array[i] = 1;
+        argument.array[i] = value;
     }
 
     int code = semctl(semid, sem_num, SETALL, argument);
@@ -41,39 +39,75 @@ int initialize_semaphores(int semid, int sem_num) {
     }
 }
 
-int get_ready_semaphores(char *file_path, int sem_flags, int sem_num) {
-    int semid = create_semaphores(file_path, sem_flags, sem_num);
-    initialize_semaphores(semid, sem_num);
+int get_ready_semaphores(int sem_num) {
+    int semid = create_semaphores(ARB_FILE, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR, sem_num);
+    initialize_semaphores(semid, sem_num, 0);
 
     return semid;
 }
 
 void check_sem_th_limit(int semid, int sem_th){
-	const int IGNORED_VALUE = 0;
-
 	struct semid_ds info;
+
 	if(semctl(semid, IGNORED_VALUE, IPC_STAT, &info) != -1){
-		if(sem_th >= info.sem_nsems){
-			fprintf(stderr, "Was specified a sem_th bigger than the array");
-		}
+        assert(sem_th < info.sem_nsems);
 	} else {
 		perror("Couldn't get information about semaphore");
 	
 	}
 }
 
-void binary_semaphore_up(int semid, int sem_th) {
+void get_sem_th_val(int semid, int sem_th){
+	struct semid_ds info;
+    int sem_val = semctl(semid, sem_th, GETVAL, &info);
+
+	if(sem_val != -1){
+        fprintf(stderr,
+                "---------------------------------------------------------\n"
+                "current status: semid: %d, sem_th: %d, sem_val: %d\n" 
+                "---------------------------------------------------------\n",
+                semid,
+                sem_th,
+                sem_val);
+	} else {
+		perror("Couldn't get information about semaphore");
+	
+	}
+}
+
+void up(int semid, int sem_th) {
 	check_sem_th_limit(semid, sem_th);
 
     struct sembuf operations[1];
 
     operations[0].sem_num = sem_th;
     operations[0].sem_op = 1;
+    operations[0].sem_flg = 0;
 
+   fprintf(stderr, "Semaphore was incremented\n");
     if(semop(semid, operations, 1) != -1){
-       fprintf(stderr, "Semaphore was incremented\n");
+        get_sem_th_val(semid, sem_th);
     } else {
        perror("Wasn't possible to up the semaphore");
+	}
+}
+
+void down(int semid, int sem_th) {
+	check_sem_th_limit(semid, sem_th);
+
+    struct sembuf operations[1];
+
+    operations[0].sem_num = sem_th;
+    operations[0].sem_op = -1;
+    operations[0].sem_flg = 0;
+
+    fprintf(stderr, "Semaphore will be decremented\n");
+    get_sem_th_val(semid, sem_th);
+
+    if(semop(semid, operations, 1) != -1){
+        // Nothing
+    } else {
+       perror("Wasn't possible to down the semaphore");
 	}
 }
 
@@ -84,25 +118,12 @@ void semaphore_wait_for_zero(int semid, int sem_th) {
 
     operations[0].sem_num = sem_th;
     operations[0].sem_op = 0;
+    operations[0].sem_flg = 0;
 
     if(semop(semid, operations, 1) != -1){
        fprintf(stderr, "Semaphore is waiting for zero\n");
+        get_sem_th_val(semid, sem_th);
     } else {
        perror("Wasn't possible to make semaphore wait for zero");
-	}
-}
-
-void binary_semaphore_down(int semid, int sem_th) {
-	check_sem_th_limit(semid, sem_th);
-
-    struct sembuf operations[1];
-
-    operations[0].sem_num = sem_th;
-    operations[0].sem_op = -1;
-
-    if(semop(semid, operations, 1) != -1){
-       fprintf(stderr, "Semaphore was decremented\n");
-    } else {
-       perror("Wasn't possible to down the semaphore");
 	}
 }
