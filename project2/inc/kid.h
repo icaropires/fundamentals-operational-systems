f69e58f6944427a4584c594aa1dcea4666f44109
ArthurTemporim <arthurrtl@gmail.com>
2017-10-11 01:12:26 -0300

    Add delay in kid.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- project2/inc/kid.h
+++ project2/inc/kid.h
@@ -1,14 +1,27 @@
 #ifndef KID_H
 #define KID_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+// Messages.
 #define ACROSSING_MSG "Kid acrossing"
 #define CROSSED_MSG "Kid crossed"
+#define THINKING_MSG "Kid thinking"
+#define WAITING_MSG "Kid waiting"
 
-void reflecting_kid();
+// Consts.
+#define MAX_THINKING_DELAY 1000
+#define MAX_CROSSING_DELAY 70
 
-void wainting_kid();
+void thinking_kid(int pid);
 
-void acrossing_kid();
+void wainting_kid(int pid);
+
+void acrossing_kid(int pid);
 
 void fall_kid();
 
--- project2/src/kid.c
+++ project2/src/kid.c
@@ -1,1 +1,15 @@
 #include "kid.h"
+
+void thinking_kid(int pid) {
+    fprintf(stdout, "%d %s\n", pid, THINKING_MSG);
+    unsigned int seed = clock();
+    int delay = rand_r(&seed)%MAX_THINKING_DELAY;
+    usleep(delay);
+}
+
+void acrossing_kid(int pid) {
+    fprintf(stdout, "%d %s\n", pid, ACROSSING_MSG);
+    unsigned int seed = clock();
+    int delay = rand_r(&seed)%MAX_CROSSING_DELAY;
+    usleep(delay);
+}
