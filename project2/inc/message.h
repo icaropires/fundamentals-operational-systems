09d0a9ba6097f1b2926d33e6c0dedebd0e4e5a55
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-10 22:17:16 -0300

    Add basic structure for project 2, reusing functions of project 1
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

---
+++ project2/inc/message.h
@@ -0,0 +1,25 @@
+#ifndef PROJECT2_INC_MESSAGE_H_
+#define PROJECT2_INC_MESSAGE_H_
+
+#include <sys/msg.h>
+
+#include "utils.h"
+
+#define NORMAL_MESSAGE_TYPE 1
+#define MESSAGE_SIZE 101
+#define FLAG 0
+
+typedef struct Message {
+    long mtype;
+    char msg[MESSAGE_SIZE];
+} Message;
+
+int create_message_queue(char *file_path, char ch);
+
+int receiving_message(int msqid, Message *msg, int arb_number, int flag);
+
+void sending_message(int msqid, Message *msg, int flag);
+
+void delete_message_queue(int msqid);
+
+#endif  // PROJECT2_INC_MESSAGE_H_
---
+++ project2/inc/utils.h
@@ -0,0 +1,18 @@
+#ifndef PROJECT2_INC_UTILS_H_
+#define PROJECT2_INC_UTILS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define ARB_NUMBER 1
+#define ARB_FILE "../.arb_file"
+#define ARB_CHAR_A 'a'
+#define PWD getenv("PWD")
+
+void generate_arb_file();
+
+char *remove_last_from_path(char* str);
+
+#endif  // PROJECT2_INC_UTILS_H_
