258772b7a604bc437efbe2d5eabc79f1ed57de1f
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-12 16:44:42 -0300

    Add function to deallocate shared memory area
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- project2/inc/shared_memory.h
+++ project2/inc/shared_memory.h
@@ -7,6 +7,8 @@
 
 int allocate_sh_memory(size_t size);
 
-pid_t* attach_sh_memory_segment(int segment_id, size_t expected_size);
+pid_t* attach_sh_memory_segment(int segment_id);
+
+void deallocate_sh_memory(int segment_id);
 
 #endif  // PROJECT2_INC_SHARED_MEMORY_H_
--- project2/src/shared_memory.c
+++ project2/src/shared_memory.c
@@ -12,21 +12,33 @@ int allocate_sh_memory(size_t size){
         fprintf(stderr, "Shared memory area creared: %d\n", segment_id);
     } else {
         perror("Couldn't create a shared memory area");
+		exit(1);
     }
 
     return segment_id;
 }
 
-pid_t* attach_sh_memory_segment(int segment_id, size_t expected_size){
+pid_t* attach_sh_memory_segment(int segment_id){
     fprintf(stderr, "\nAttaching to shared memory area...\n");
 
     pid_t *shared_memory = (pid_t*) shmat(segment_id, NULL, 0);
 
     if(shared_memory != (pid_t*) -1){
-        fprintf(stderr, "%d to shared memory area area.\n", getpid());
+        fprintf(stderr, "%d to shared memory area.\n", getpid());
     } else {
         perror("%d couldn't attach to shared memory area");
+		exit(1);
     }
 
     return shared_memory;
+}
+
+void deallocate_sh_memory(int segment_id){
+	fprintf(stderr, "\nDeallocting shared memory %d\n", segment_id);
+
+	if(shmctl(segment_id, IPC_RMID, 0) != -1){
+		fprintf(stderr, "Shared memory deallocated\n");
+	} else {
+		perror("Couldn't deallocate shared_memory");
+	}
 }
