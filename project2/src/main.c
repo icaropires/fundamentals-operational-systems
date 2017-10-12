679def38eecdb02e43901291388d415b5e209483
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-12 18:56:50 -0300

    Make children cross the rope
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- project2/src/main.c
+++ project2/src/main.c
@@ -1,6 +1,7 @@
 #include "../inc/semaphore.h"
 #include "../inc/message.h"
 #include "../inc/kid.h"
+#include "../inc/shared_memory.h"
 
 int main(int argc, char** argv) {
 	generate_arb_file();
@@ -9,28 +10,58 @@ int main(int argc, char** argv) {
 	fprintf(stdout, "Quantas crianças?\n");
 	scanf("%d", &n_kids);
 
-	pid_t rope[STEPS_TO_CROSS];
-	memset(rope, -1, sizeof(rope));
+	int semid = get_ready_semaphores(STEPS_TO_CROSS, 1);
 
-	get_ready_semaphores(STEPS_TO_CROSS, 1);
+	int segment_id = allocate_sh_memory(STEPS_TO_CROSS);
+	pid_t *rope = attach_sh_memory_segment(segment_id);
+	memset(rope, -1, sizeof(*rope) * STEPS_TO_CROSS);
 
 	for(int i = 0; i < n_kids; ++i){
 		pid_t pid = fork();
 
-		if(pid){
+		if(pid && i == 0){
 			fprintf(stderr, "Starting parent process: %d\n", getpid());
 
-			fprintf(stderr, "Ending parent\n");
-		} else{
-			pid_t my_pid = getpid();
+			const int each_step_time = MAX_CROSSING_DELAY/STEPS_TO_CROSS;
+			
+			printf("Meu pid: %d\n", getpid());
 
-			fprintf(stdout, "Starting kid (%d): %d\n", i, my_pid);
+			for(int i = 0; i <= STEPS_TO_CROSS; ++i){
+				usleep(2000);
+				fflush(stdout);
+				print_rope(rope, STEPS_TO_CROSS);
+			}
+
+			// Waiting for children to die
+			for(int j = n_kids; j > 0; --j){
+				int status = 3;
+				wait(&status);
+
+				if(status == 0){
+					fprintf(stderr, "Some kid returned succesfully\n");
+				} else {
+					fprintf(stderr, "Some kid returned an error\n");
+				}
+			}
+
+			remove_semaphores(semid);
+			deallocate_sh_memory(segment_id);
+
+			fprintf(stderr, "Ending parent\n");
+		} else if (!pid){
+			printf("criança %d\n", getpid());
+
+			pid_t my_pid = getpid();
+			fprintf(stderr, "Starting kid (%d): %d\n", i, my_pid);
+
+			pid_t *rope = attach_sh_memory_segment(segment_id);
 
 			kid_think(my_pid);
 			// if can cross
 			kid_cross(my_pid, rope);
 
 			fprintf(stderr, "Ending kid\n");
+			exit(0);
 		}
 	}
 			
