3dfa245db6e2e5c5807ae888e4440d6355bfd8f7
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-12 00:52:06 -0300

    Add structure for kid crossing
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- project2/src/kid.c
+++ project2/src/kid.c
@@ -1,26 +1,43 @@
-#include "kid.h"
+#include "../inc/kid.h"
 
-void thinking_kid(int pid) {
+void kid_think(pid_t pid) {
     print_kid_status(pid, THINKING_MSG);
     apply_delay(MAX_THINKING_DELAY);
 }
 
-void waiting_kid(int pid) {
+void kid_wait(pid_t pid) {
     print_kid_status(pid, WAITING_MSG);
-    
 }
 
-void acrossing_kid(int pid) {
-    print_kid_status(pid, ACROSSING_MSG);
+void kid_cross(pid_t pid, pid_t *rope) {
+    print_kid_status(pid, CROSSING_MSG);
+
+    int semid = get_ready_semaphores(STEPS_TO_CROSS, 0);
+	assert(get_sem_size(semid) == STEPS_TO_CROSS);
+
+    for(int i = 0; i < STEPS_TO_CROSS; ++i){
+        // Erase foot print of previus step
+        if(rope[i - (i != 0)] && rope[i] == pid){
+            rope[i-1] = -1;
+        }
+
+        down(semid, i);
+        rope[i] = pid;
+        apply_delay((float)MAX_CROSSING_DELAY/STEPS_TO_CROSS);
+        up(semid, i);
+    }
+
     apply_delay(MAX_CROSSING_DELAY);
 }
 
-void apply_delay(int time) {
+void apply_delay(time_t time) {
     unsigned int seed = clock();
-    int delay = rand_r(&seed)%time;
+    assert(seed != -1);
+
+    int delay = rand_r(&seed) % time;
     usleep(delay);
 }
 
-void print_kid_status(int id, char *status) {
-    fprintf(stdout, "%d %s\n", id, status);
+void print_kid_status(pid_t pid, char *status) {
+    fprintf(stdout, "%d %s\n", pid, status);
 }
--- project2/src/main.c
+++ project2/src/main.c
@@ -1,22 +1,39 @@
-#include "semaphore.h"
-#include "message.h"
-#include "kid.h"
+#include "../inc/semaphore.h"
+#include "../inc/message.h"
+#include "../inc/kid.h"
 
 int main(int argc, char** argv) {
 	generate_arb_file();
-	int semid = get_ready_semaphores(10);
 
-    pid_t pid = fork();
+	int n_kids = 0;
+	fprintf(stdout, "Quantas crianças, meu chapa?\n");
+	scanf("%d", &n_kids);
 
-	if(pid){
-		remove_semaphores(semid);
-		fprintf(stderr, "Ending parent\n");
-	} else{
-		fprintf(stderr, "Ending kid\n");
+	pid_t rope[STEPS_TO_CROSS];
+	memset(rope, -1, sizeof(rope));
+
+	int semid = get_ready_semaphores(STEPS_TO_CROSS, 1);
+	fprintf(stderr, "Initialized semaphore with semid: %d\n", semid);
+
+	for(int i = 0; i < n_kids; ++i){
+		pid_t pid = fork();
+
+		if(pid){
+			fprintf(stderr, "Starting parent process: %d\n", getpid());
+
+			fprintf(stderr, "Ending parent\n");
+		} else{
+			pid_t my_pid = getpid();
+
+			fprintf(stderr, "Starting kid (%d): %d\n", i, my_pid);
+
+			kid_think(my_pid);
+			// if can cross
+			kid_cross(my_pid, rope);
+
+			fprintf(stderr, "Ending kid\n");
+		}
 	}
+			
 	return 0;
-    // thinking_kid(pid);
-    // waiting_kid(pid);
-    // acrossing_kid(pid);
-	
 }
