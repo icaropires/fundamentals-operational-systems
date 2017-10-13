a858ccff90bf74c6309108b4ec45ace845455d98
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-13 00:37:12 -0300

    Add function to follow crosses
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- project2/inc/kid.h
+++ project2/inc/kid.h
@@ -16,8 +16,12 @@
 #define WAITING_MSG "Kid is waiting"
 
 #define MAX_THINKING_DELAY 1000
-#define MAX_CROSSING_DELAY 70
+#define MAX_CROSSING_DELAY 70 * (int)1e5 * 2
 #define STEPS_TO_CROSS 7
+#define ROPE_SIZE (STEPS_TO_CROSS + 1)
+#define AMOUNT_CROSSED 0 // Index to amount of kids passing
+
+void apply_random_delay(time_t time);
 
 void kid_think(pid_t pid);
 
@@ -25,11 +29,11 @@ void kid_wait(pid_t pid);
 
 void kid_cross(pid_t pid, pid_t *rope);
 
-void print_rope(pid_t *rope, size_t size);
+void print_rope(pid_t *rope);
+
+void watch_printing_rope(pid_t *rope, int n_crosses);
 
 void fall_kid();
-
-void apply_delay(time_t time);
 
 void print_kid_status(pid_t pid, char *status);
 
--- project2/src/kid.c
+++ project2/src/kid.c
@@ -1,10 +1,27 @@
 #include "../inc/kid.h"
 
+// time is the maximum value
+void apply_random_delay(time_t time) {
+    fprintf(stderr, "\n(start) Applying delay of %d miliseconds..\n", (int)time);
+
+    unsigned int seed = clock();
+    assert(seed != -1);
+
+    int delay = rand_r(&seed) % time;
+    usleep(delay);
+
+    fprintf(stderr, "(success) Delay of %d miliseconds ended\n", (int)time);
+}
+
+void print_kid_status(pid_t pid, char *status) {
+    fprintf(stderr, "%d %s\n", pid, status);
+}
+
 void kid_think(pid_t pid) {
     fprintf(stderr, "\n(start) Kid %d starting to think...\n", pid);
 
     print_kid_status(pid, THINKING_MSG);
-    apply_delay(MAX_THINKING_DELAY);
+    apply_random_delay(MAX_THINKING_DELAY);
 
     fprintf(stderr, "(success) Kid %d ended thinking\n", pid);
 }
@@ -20,47 +37,57 @@ void kid_wait(pid_t pid) {
 void kid_cross(pid_t pid, pid_t *rope) {
     fprintf(stderr, "\n(start) Kid %d starting thinking...\n", pid);
 
-    int semid = get_ready_semaphores(STEPS_TO_CROSS, 0);
-	assert(get_sem_size(semid) == STEPS_TO_CROSS);
+    int semid = get_ready_semaphores(ROPE_SIZE, 0, 1);
+	assert(get_sem_size(semid) == ROPE_SIZE);
 
-    for(int i = 0; i < STEPS_TO_CROSS; ++i){
+    for(int i = 1; i < ROPE_SIZE; ++i){
         // Erase footprint of previus step
-        if(rope[i - (i != 0)] && rope[i] == pid){
-            rope[i-1] = -1;
+        if(i > 1){
+			if(rope[i - 1] == pid){
+				rope[i-1] = -1;
+			}
         }
 
         down(semid, i);
         rope[i] = pid;
-        apply_delay((float)MAX_CROSSING_DELAY/STEPS_TO_CROSS);
+        usleep(MAX_CROSSING_DELAY/STEPS_TO_CROSS);
+		if(i == ROPE_SIZE - 1){
+			rope[i] = -1;
+		}
         up(semid, i);
     }
+
+	rope[AMOUNT_CROSSED]++;
 
     fprintf(stderr, "(success) Kid %d crossed\n", pid);
 }
 
-void print_rope(pid_t *rope, size_t size){
+void print_rope(pid_t *rope){
     fprintf(stderr, "\n(start) Printing rope...\n");
 
-	for(int i = 0; i < size; ++i){
-		fprintf(stdout, "[%d]%s",
-				(int) rope[i], i != size - 1? "" : "\n");
+	for(int i = 1; i < ROPE_SIZE; ++i){
+		fprintf(stdout, "[%4d]%s",
+				(int) rope[i], i != ROPE_SIZE - 1? "" : "\n");
 	}
 
     fprintf(stderr, "(success) Rope was printed\n");
 }
 
-void apply_delay(time_t time) {
-    fprintf(stderr, "\n(start) Applying delay of %d miliseconds..\n", (int)time);
 
-    unsigned int seed = clock();
-    assert(seed != -1);
+void watch_printing_rope(pid_t *rope, int n_crosses){
+	fprintf(stdout, "\n=========================================================\n");
 
-    int delay = rand_r(&seed) % time;
-    usleep(delay);
+	time_t time_counter = 0;
+	time_t time_step = MAX_CROSSING_DELAY/STEPS_TO_CROSS;
 
-    fprintf(stderr, "(success) Delay of %d miliseconds ended\n", (int)time);
-}
+	while(rope[AMOUNT_CROSSED] < n_crosses){
+		fprintf(stdout, "%10d ms: ", (int)time_counter);
+		print_rope(rope);
 
-void print_kid_status(pid_t pid, char *status) {
-    fprintf(stderr, "%d %s\n", pid, status);
+		usleep(time_step);
+		time_counter += time_step;
+	}
+	fprintf(stdout, "%10d ms: ", (int)time_counter);
+	print_rope(rope);
+	fprintf(stdout, "=========================================================\n");
 }
