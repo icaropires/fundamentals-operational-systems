8604db6dadaae441138a7cd6bc294acfd318836c
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-15 15:54:35 -0200

    Make multiple crosses switching sides
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- project2/inc/kid.h
+++ project2/inc/kid.h
@@ -9,31 +9,40 @@
 #include "../inc/utils.h"
 #include "../inc/semaphore.h"
 
-// Messages.
+// Messages
 #define CROSSING_MSG "Kid is crossing"
 #define CROSSED_MSG "Kid has crossed"
 #define THINKING_MSG "Kid is thinking"
-#define WAITING_MSG "Kid is waiting"
 
+// About crossing
 #define STEPS_TO_CROSS 7
 #define ROPE_SIZE (STEPS_TO_CROSS + 2)
 #define LAST_STEP (FIRST_STEP + STEPS_TO_CROSS - 1)
 #define FIRST_STEP 1
 
-#define MAX_THINKING_DELAY 1000
-#define MAX_CROSSING_DELAY 70 * (int)1e3
-#define STEP_DELAY (MAX_CROSSING_DELAY/STEPS_TO_CROSS)
+// Delays
+#define MAX_THINKING_DELAY 1000  // micro-seconds
+#define MAX_CROSSED_DELAY 70 * (int)2e4 // micro-seconds
+#define STEP_DELAY (MAX_CROSSED_DELAY/STEPS_TO_CROSS) // micro-seconds
+#define SWITCH_SIDES_DELAY (int)5e3 // mico-seconds
+
+// Sides
 #define RIGHT 'R'
 #define LEFT 'L'
 
-#define META_SIZE 3
-#define RIGHT_AMOUNT 0
-#define LEFT_AMOUNT 1
-#define CROSSING_AMOUNT 2
+// About meta shared
+#define META_SIZE 4
+#define RIGHT_AMOUNT 0  // Index
+#define LEFT_AMOUNT 1  // Index
+#define CROSSED_AMOUNT 2  // Index
+#define CROSSING_AMOUNT 3  // Index
 
+// About locking
 #define LOCKING_MANAGE_SIZE 2
-#define RIGHT_LOCK 0
-#define LEFT_LOCK 1
+#define RIGHT_LOCK 0  // Index
+#define LEFT_LOCK 1  // Index
+
+#define EXTRA_CHILD_PROCESS_AMOUNT 2 // Watch and switch
 
 typedef struct kid {
 	pid_t pid;
--- project2/src/main.c
+++ project2/src/main.c
@@ -8,59 +8,97 @@ int main(int argc, char** argv) {
 
 	generate_arb_file();
 
+	// Interacting with user
 	int n_kids = 0;
 	fprintf(stdout, "Quantas crianças?\n");
 	scanf("%d", &n_kids);
 
+	// Prepare shared memory area with kids info
     int kids_segment_id = allocate_sh_memory(n_kids);
     Kid *kids = (Kid *) attach_sh_memory_segment(kids_segment_id);
     memset(kids, -1, n_kids * sizeof(Kid));
 
+	// Prepare shared memory area which is the rope
 	int rope_segment_id = allocate_sh_memory(ROPE_SIZE);
 	pid_t *rope = (pid_t *) attach_sh_memory_segment(rope_segment_id);
 	memset(rope, -1, ROPE_SIZE * sizeof(pid_t));
 
+	// Prepare shared memory area for managing kids crossing
 	int manage_segment = allocate_sh_memory(META_SIZE);
 	int *meta_sh = (int *) attach_sh_memory_segment(manage_segment);
 	memset(meta_sh, -1, META_SIZE * sizeof(int));
 
+	// Getting semaphores for managing kids and locking sides
 	int sem_rope = get_ready_semaphores(ROPE_SIZE, 1, 1, ARB_CHAR_A);
 	int locking_manage = get_ready_semaphores(LOCKING_MANAGE_SIZE, 1, 1, ARB_CHAR_B);
 
+	// Initializing what is needed in shared memory area
+    meta_sh[CROSSED_AMOUNT] = 100;
     meta_sh[CROSSING_AMOUNT] = 0;
 
-    // down(locking_manage, RIGHT_LOCK);
-    down(locking_manage, LEFT_LOCK);
+	// Starts with one side locked
+	down(locking_manage, LEFT_LOCK);
 
+	// Generate the multiple kids
 	for(int i = 0; i < n_kids; ++i){
 		pid_t pid = fork();
 
+		// Process parent
 		if(pid && i == 0){
 			fprintf(stderr, "Starting parent process: %d\n", getpid());
 
+			// Process for watching
 			pid_t watch_pid = fork();
 			if(watch_pid == 0){
 				watch_printing_rope(rope, n_kids, kids, meta_sh);
 				exit(0);
 			}
 
+			// Process for swap sides
+			pid_t swap_sides_pid = fork();
+			if(swap_sides_pid == 0){
+				int *meta_sh = attach_sh_memory_segment(manage_segment);
+
+				while(meta_sh[CROSSED_AMOUNT]){
+					usleep(SWITCH_SIDES_DELAY);
+					down(locking_manage, RIGHT_LOCK);
+					while(meta_sh[CROSSING_AMOUNT]){
+						usleep(STEP_DELAY);
+					}
+					up(locking_manage, LEFT_LOCK);
+
+					usleep(SWITCH_SIDES_DELAY);
+					down(locking_manage, LEFT_LOCK);
+					while(meta_sh[CROSSING_AMOUNT]){
+						usleep(STEP_DELAY);
+					}
+					up(locking_manage, RIGHT_LOCK);
+				}
+				exit(0);
+			}
+
 			fprintf(stderr, "Ending parent\n");
-		} else if (!pid){
+		} else if (!pid){  // Kid process
 			pid_t my_pid = getpid();
 			fprintf(stderr, "Starting kid (%d): %d\n", i + 1, my_pid);
 
+			// Attaching to shared memory areas
 			pid_t *rope = attach_sh_memory_segment(rope_segment_id);
 			int *meta_sh = attach_sh_memory_segment(manage_segment);
 			Kid *kids = attach_sh_memory_segment(kids_segment_id);
 
-            if (i < 3) {
+			// Generate kid for each side
+            if (i % 2) {
                 fill_kid_info(my_pid, kids, n_kids, RIGHT);
             } else {
                 fill_kid_info(my_pid, kids, n_kids, LEFT);
             }
 
-            Kid this_kid = kids[find_kid(my_pid, kids, n_kids)];
+			// Know what the index of this kid on kids vector
+			int this_kid_index = find_kid(my_pid, kids, n_kids);
+            Kid this_kid = kids[this_kid_index];
 
+			// Crossing
 			kid_think(this_kid);
 			kid_cross(this_kid, rope, meta_sh);
 
@@ -68,20 +106,20 @@ int main(int argc, char** argv) {
 			exit(0);
 		}
 	}
-
-	// Plus 1 because of watching process
-	for(int i = 0; i < n_kids + 1; ++i){
+	
+	// Wait for kids finish
+	for(int i = 0; i < n_kids + EXTRA_CHILD_PROCESS_AMOUNT; ++i){
 		wait(0);
 	}
 
+	// Freeing resources
     fprintf(stderr, "\n(start) Final deallocations...\n");
 	remove_semaphores(sem_rope);
 	remove_semaphores(locking_manage);
-
 	deallocate_sh_memory(rope_segment_id);
 	deallocate_sh_memory(kids_segment_id);
 	deallocate_sh_memory(manage_segment);
-    fprintf(stderr, "\n(success) Final deallocations...\n");
+    fprintf(stderr, "(success) Final deallocations...\n");
 
     fprintf(stderr, "(success) Everything was like it meant to be\n");
 	return 0;
