05d4b096914a09f2e53bd2641079b1b6e926efb8
ArthurTemporim <arthurrtl@gmail.com>
2017-11-09 19:46:13 -0200

    Make necessarys changes.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- rpc/calcula_server.c
+++ rpc/calcula_server.c
@@ -10,9 +10,8 @@ int * add_100_svc(operandos *argp, struct svc_req *rqstp)
 {
 	static int  result;
 
-	/*
-	 * insert server code here
-	 */
+	printf("Requisicao de adicao para %d e %d\n", argp->x, argp->y);
+	result = argp->x + argp->y;
 
 	return &result;
 }
@@ -21,9 +20,8 @@ int * sub_100_svc(operandos *argp, struct svc_req *rqstp)
 {
 	static int  result;
 
-	/*
-	 * insert server code here
-	 */
+	printf("Requisicao de subtracao para %d e %d\n\n", argp->x, argp->y);
+	result = argp->x - argp->y;
 
 	return &result;
 }
