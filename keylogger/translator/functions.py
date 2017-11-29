f4cab5b39ad7a0965d0f9e0dae1174d19b3576d2
Ícaro Pires <icaropiresdf@gmail.com>
2017-11-29 17:17:55 -0200

    Fix codes for arrows
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- keylogger/translator/functions.py
+++ keylogger/translator/functions.py
@@ -4,7 +4,8 @@ from scancode_constants import (UNPRESS_INTERVAL,
                                 SPACE,
                                 ENTER,
                                 CAPS_LOCK,
-                                IGNORED_KEYS)
+                                IGNORED_KEYS,
+                                SPECIAL_KEY)
 
 RESULTS_FILE = '../io/results.txt'
 SCANCODE_COLUMN = 3
@@ -57,7 +58,6 @@ def substitute_keys(key_dict, data):
                   .format(i/len(data)*100)) # 100 because is percentage
 
         key = data[i]
-
         prev_key = data[i - 1]
 
         if key == SPACE:
@@ -74,8 +74,22 @@ def substitute_keys(key_dict, data):
                 caps_key = data[i]
         elif (int(key) != int(prev_key) + UNPRESS_INTERVAL) and \
             key not in IGNORED_KEYS:
+
             if key in key_dict.keys():
-                new_data += key_dict[key]
+                # Treating arrows
+                if prev_key == SPECIAL_KEY:
+                    if key == '75':
+                        new_data += '[LEFTARROW]'
+                    elif key == '77':
+                        new_data += '[RIGHTARROW]'
+                    elif key == '80':
+                        new_data += '[DOWNARROW]'
+                    elif key == '72':
+                        new_data += '[UPARROW]'
+                    else: 
+                        continue
+                else:
+                    new_data += key_dict[key]
 
     print('Keys substituted successfully!')
     return new_data
